15f981b89b82b2998b23052577c29a65ea924064
trungnd_b01414 <trungnd_b01414@3b5cc029-b0f4-4ce7-9e8c-39093b747338>
2013-09-05 15:26:44 +0000

    Recreate Utils class
    git-svn-id: https://aptech.ac.vn:8443/svn/fat2-c1203l-s2-g2@26 3b5cc029-b0f4-4ce7-9e8c-39093b747338

--- SourceCode/client/src/client/Utils.java
+++ SourceCode/client/src/client/Utils.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package gui;
+package client;
 
 import java.awt.Component;
 import javax.swing.JOptionPane;
