fd47151891ce284073139af0d702d30cd5947001
trungnd_b01414 <trungnd_b01414@3b5cc029-b0f4-4ce7-9e8c-39093b747338>
2013-09-05 17:13:10 +0000

    Introduce isAuthorizedWithSideEffect
    git-svn-id: https://aptech.ac.vn:8443/svn/fat2-c1203l-s2-g2@30 3b5cc029-b0f4-4ce7-9e8c-39093b747338

--- SourceCode/common/src/rbac/SessionManager.java
+++ SourceCode/common/src/rbac/SessionManager.java
@@ -4,19 +4,44 @@
  */
 package rbac;
 
+import java.rmi.AccessException;
+
 /**
  *
  * @author chin
  */
 public class SessionManager {
+
     private Session singletonSession;
-    
+
+    /**
+     * Check if a session has a certain permission.
+     */
     public boolean isAuthorized(String sessionId, Permission permission) {
-        for (Role r : singletonSession.getRoles())
-            for (Permission p : r.getPermissions())
-                if (p.equals(permission))
+        for (Role r : singletonSession.getRoles()) {
+            for (Permission p : r.getPermissions()) {
+                if (p.equals(permission)) {
                     return true;
+                }
+            }
+        }
         return false;
+    }
+
+    
+    /**
+     * Similar to isAuthorized but returns nothing and throws an AccessException on failure.
+     * @throws AccessException
+     */
+    public void isAuthorizedWithSideEffect(
+            String sessionId,
+            Permission permission) throws AccessException {
+        if (!isAuthorized(sessionId, permission)) {
+            throw new AccessException(String.format(
+                    "You can't %s %s",
+                    permission.getAction(),
+                    permission.getTarget()));
+        }
     }
 
     public void add(Session session) {
