95c23566c15cc0c05987d139176e00191682c897
trungnd_b01414 <trungnd_b01414@3b5cc029-b0f4-4ce7-9e8c-39093b747338>
2013-09-06 10:02:13 +0000

    Create an info message when the user login
    git-svn-id: https://aptech.ac.vn:8443/svn/fat2-c1203l-s2-g2@39 3b5cc029-b0f4-4ce7-9e8c-39093b747338

--- SourceCode/client/src/client/LoginDialog.java
+++ SourceCode/client/src/client/LoginDialog.java
@@ -8,7 +8,10 @@ import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.util.List;
 import javax.swing.JOptionPane;
+import rbac.Permission;
+import rbac.Role;
 import rbac.Session;
 import rmi.IAuthenticator;
 
@@ -120,6 +123,26 @@ public class LoginDialog extends javax.swing.JDialog {
                     (IAuthenticator) Naming.lookup("rmi://localhost/authenticator");
             session = this.authenticator.login(username, password);
             if (session != null) {
+                List<Role> roles = session.getRoles();
+                StringBuilder roleString = new StringBuilder();
+                StringBuilder permString = new StringBuilder();
+                
+                for (Role role : roles) {
+                    roleString.append(role.getName());
+                    roleString.append(",");
+                    
+                    for (Permission p : role.getPermissions()) {
+                        permString.append(p.getAction());
+                        permString.append(" ");
+                        permString.append(p.getTarget());
+                        permString.append(", ");
+                    }
+                }
+                
+                Utils.showInfoDialog(this,
+                        String.format("You are %s\nand can %s.",
+                            roleString.toString(),
+                            permString.toString()));
                 this.setVisible(false);
             } else {
                 Utils.showErrorDialog(this,
