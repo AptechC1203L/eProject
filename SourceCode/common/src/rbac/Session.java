1d64c2558ef8df474c10f918c5728b6aeb8269ff
trungnd_b01414 <trungnd_b01414@3b5cc029-b0f4-4ce7-9e8c-39093b747338>
2013-09-10 11:06:23 +0000

    Implement Session.getAllPermissions()
    git-svn-id: https://aptech.ac.vn:8443/svn/fat2-c1203l-s2-g2@79 3b5cc029-b0f4-4ce7-9e8c-39093b747338

--- SourceCode/common/src/rbac/Session.java
+++ SourceCode/common/src/rbac/Session.java
@@ -5,6 +5,7 @@
 package rbac;
 
 import java.io.Serializable;
+import java.util.LinkedList;
 import java.util.List;
 import lombok.Data;
 
@@ -17,4 +18,14 @@ public class Session implements Serializable {
     private final String sessionId;
     private final User user;
     private final List<Role> roles;
+    
+    public List<Permission> getAllPermissions() {
+        LinkedList<Permission> permissionList = new LinkedList<>();
+        for (Role r : getRoles()) {
+            for (Permission p : r.getPermissions()) {
+                permissionList.add(p);
+            }
+        }
+        return permissionList;
+    }
 }
