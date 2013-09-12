bdf26d33d7fdb60e6fa0c6d674e73385162ce68b
Trung Ngo <ndtrung4419@gmail.com>
2013-09-12 17:05:59 +0700

    Make due_date field in table Orders nullable

--- Resource/clean_database.sql
+++ Resource/clean_database.sql
@@ -2,7 +2,7 @@
 
 
 CREATE TABLE [Roles] (
-  [name] varchar(20) NOT NULL , 
+  [name] varchar(20) NOT NULL ,
  PRIMARY KEY ([name])
 ) ON [PRIMARY]
 GO
@@ -17,7 +17,7 @@ CREATE TABLE [Orders] (
   [weight] float NOT NULL ,
   [description] ntext ,
   [timestamp] timestamp NOT NULL ,
-  [due_data] datetime NOT NULL , 
+  [due_date] datetime ,
  PRIMARY KEY ([id])
 ) ON [PRIMARY]
 GO
@@ -27,41 +27,41 @@ CREATE TABLE [Users] (
   [name] nvarchar(100) NOT NULL ,
   [honorific] nvarchar(100) ,
   [about_me] ntext ,
-  [phone] varchar(100) , 
+  [phone] varchar(100) ,
  PRIMARY KEY ([username])
 ) ON [PRIMARY]
 GO
 
 CREATE TABLE [UserRoleMapping] (
   [username] varchar(20) NOT NULL ,
-  [role] varchar(20) NOT NULL 
+  [role] varchar(20) NOT NULL
 ) ON [PRIMARY]
 GO
 
 CREATE TABLE [Permissions] (
   [id] int IDENTITY (1, 1) ,
   [action] varchar(20) NOT NULL ,
-  [target] varchar(200) NOT NULL , 
+  [target] varchar(200) NOT NULL ,
  PRIMARY KEY ([id])
 ) ON [PRIMARY]
 GO
 
 CREATE TABLE [RolePermissionMapping] (
   [role_name] varchar(20) NOT NULL ,
-  [permission_id] int NOT NULL 
+  [permission_id] int NOT NULL
 ) ON [PRIMARY]
 GO
 
 CREATE TABLE [Prices] (
   [established_date] datetime NOT NULL ,
-  [price_per_kilogram] money NOT NULL , 
+  [price_per_kilogram] money NOT NULL ,
  PRIMARY KEY ([established_date])
 ) ON [PRIMARY]
 GO
 
 CREATE TABLE [Log] (
   [timestamp] timestamp NOT NULL ,
-  [content] ntext , 
+  [content] ntext ,
  PRIMARY KEY ([timestamp])
 ) ON [PRIMARY]
 GO
@@ -70,23 +70,23 @@ CREATE TABLE [WorkJournal] (
   [date] datetime ,
   [username] varchar(20) NOT NULL ,
   [status] varchar(20) NOT NULL ,
-  [reason] ntext , 
+  [reason] ntext ,
  PRIMARY KEY ([date])
 ) ON [PRIMARY]
 GO
 
 ALTER TABLE [Orders] ADD FOREIGN KEY (creator_id) REFERENCES [Users] ([username]);
-				
+
 ALTER TABLE [Orders] ADD FOREIGN KEY (deliverer_id) REFERENCES [Users] ([username]);
-				
+
 ALTER TABLE [UserRoleMapping] ADD FOREIGN KEY (username) REFERENCES [Users] ([username]);
-				
+
 ALTER TABLE [UserRoleMapping] ADD FOREIGN KEY (role) REFERENCES [Roles] ([name]);
-				
+
 ALTER TABLE [RolePermissionMapping] ADD FOREIGN KEY (role_name) REFERENCES [Roles] ([name]);
-				
+
 ALTER TABLE [RolePermissionMapping] ADD FOREIGN KEY (permission_id) REFERENCES [Permissions] ([id]);
-				
+
 ALTER TABLE [WorkJournal] ADD FOREIGN KEY (username) REFERENCES [Users] ([username]);
-				
+
 
