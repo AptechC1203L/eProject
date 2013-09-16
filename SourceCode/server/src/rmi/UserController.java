c4d64700b6aa8289ea05bf56e804f1fcbf73fb4c
trungnd_b01414 <trungnd_b01414@3b5cc029-b0f4-4ce7-9e8c-39093b747338>
2013-09-16 08:45:30 +0000

    Implement UserController.createUser() and getUser()
    git-svn-id: https://aptech.ac.vn:8443/svn/fat2-c1203l-s2-g2@132 3b5cc029-b0f4-4ce7-9e8c-39093b747338

--- SourceCode/server/src/rmi/UserController.java
+++ SourceCode/server/src/rmi/UserController.java
@@ -4,10 +4,106 @@
  */
 package rmi;
 
+import db.ConnectionFactory;
+import java.rmi.RemoteException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import rbac.Permission;
+import rbac.SessionManager;
+import rbac.User;
+
 /**
  *
  * @author chin
  */
-public class UserController {
+public class UserController extends Controller implements IUserController {
+    
+    public UserController(SessionManager sessionManager,
+                             ConnectionFactory connectionFactory)
+            throws RemoteException {
+        super(sessionManager, connectionFactory);
+    }
+
+    
+    @Override
+    public User createUser(String sessionId, User user) throws RemoteException {
+        sessionManager.isAuthorizedWithSideEffect(
+                sessionId,
+                new Permission("create", "user"));
+        try (Connection conn = connectionFactory.getConnection();
+             Statement statement = conn.createStatement()) {
+            statement.executeUpdate(
+                     "INSERT INTO [Users] " +
+                     "(username, name, honorific, about_me, phone) " +
+                     "VALUES (" + serializeUser(user) + ")");
+        } catch (SQLException ex) {
+            Logger.getLogger(UserController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return user;
+    }
+
+    @Override
+    public User getUser(String sessionId, String username) throws RemoteException {
+        sessionManager.isAuthorizedWithSideEffect(
+                sessionId,
+                new Permission("view", "user"));
+        
+        User user = null;
+        try (Connection conn = connectionFactory.getConnection();
+                PreparedStatement statement = conn.prepareStatement(
+                "SELECT * from [Users] WHERE username = ?")) {
+            statement.setString(1, username);
+            
+            ResultSet result = statement.executeQuery();
+            
+            while (result.next()) {
+                user = deserializeUser(result);
+            }
+        } catch (SQLException ex) {
+            // FIXME What to do here?
+        } finally {
+            return user;
+        }
+    }
+    
+    private User deserializeUser(ResultSet row) throws SQLException {
+        String username = row.getString("username");
+        String name = row.getString("name");
+        String honorific = row.getString("honorific");
+        String about_me = row.getString("about_me");
+        String phone = row.getString("phone");
+        
+        User user = new User(username, name);
+        // TODO set the other properties
+        
+        return user;
+    }
+    
+    private String serializeUser(User user) {
+        return String.format("'%s', '%s', 'Mr', 'just me', '1234'",
+                user.getUserId(),
+                user.getName());
+    }
+
+    @Override
+    public List<User> getAllUsers(String sessionId) throws RemoteException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean updateUser(String sessionId, User newUser) throws RemoteException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean deleteUser(String SessionId, String username) throws RemoteException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
