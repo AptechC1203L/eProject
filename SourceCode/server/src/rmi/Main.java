03a1ffad8c9108bf29efbed80037932a0fd48d2b
trungnd_b01414 <trungnd_b01414@3b5cc029-b0f4-4ce7-9e8c-39093b747338>
2013-09-16 08:45:49 +0000

    Fix Main to work with the new UserController
    git-svn-id: https://aptech.ac.vn:8443/svn/fat2-c1203l-s2-g2@134 3b5cc029-b0f4-4ce7-9e8c-39093b747338

--- SourceCode/server/src/rmi/Main.java
+++ SourceCode/server/src/rmi/Main.java
@@ -7,7 +7,10 @@ package rmi;
 import db.ConnectionFactory;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+import rbac.Session;
 import rbac.SessionManager;
+import rbac.User;
 
 public class Main {
 
@@ -19,12 +22,14 @@ public class Main {
             ConnectionFactory connFactory =
                     new ConnectionFactory("localhost", 1433, "sa", "1111", "courier");
             SessionManager m = new SessionManager();
-            OrderController orderController = new OrderController(m, connFactory);
+            IOrderController orderController = new OrderController(m, connFactory);
+            IUserController userController = new UserController(m, connFactory);
             IAuthenticator authenticator = new Authenticator(m);
             UserController usercontroller = new UserController(m);
             Registry registry = LocateRegistry.createRegistry(1099);
-            
+                        
             registry.bind("orders", orderController);
+            registry.bind("users", userController);
             registry.bind("authenticator", authenticator);
             registry.bind("users", usercontroller);
             System.out.println("Server listening...");
