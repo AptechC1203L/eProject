2c0cd96179b99b5375d67ab3ca2c0f53000fe4f9
trungnd_b01414 <trungnd_b01414@3b5cc029-b0f4-4ce7-9e8c-39093b747338>
2013-09-16 09:28:38 +0000

    Update database design and include some default data
    git-svn-id: https://aptech.ac.vn:8443/svn/fat2-c1203l-s2-g2@149 3b5cc029-b0f4-4ce7-9e8c-39093b747338

--- Resource/clean_database.sql
+++ Resource/clean_database.sql
@@ -1,92 +1,125 @@
-
-
-
 CREATE TABLE [Roles] (
-  [name] varchar(20) NOT NULL ,
+  [name] varchar(20) NOT NULL , 
  PRIMARY KEY ([name])
 ) ON [PRIMARY]
 GO
 
 CREATE TABLE [Orders] (
-  [id] int IDENTITY (1, 1) ,
-  [from] nvarchar(500) NOT NULL ,
-  [to] nvarchar(500) NOT NULL ,
+  [id] int NOT NULL IDENTITY (1, 1) ,
+  [from] nvarchar(500) ,
+  [to] nvarchar(500) ,
   [creator_id] varchar(20) ,
   [deliverer_id] varchar(20) ,
   [status] varchar(20) ,
-  [weight] float NOT NULL ,
+  [weight] float ,
   [description] ntext ,
-  [timestamp] timestamp NOT NULL ,
+  [timestamp] datetime NOT NULL ,
   [due_date] datetime ,
+  [delivered_date] datetime , 
  PRIMARY KEY ([id])
 ) ON [PRIMARY]
 GO
 
 CREATE TABLE [Users] (
   [username] varchar(20) NOT NULL ,
-  [name] nvarchar(100) NOT NULL ,
+  [name] nvarchar(100) ,
   [honorific] nvarchar(100) ,
   [about_me] ntext ,
-  [phone] varchar(100) ,
+  [phone] varchar(100) , 
  PRIMARY KEY ([username])
 ) ON [PRIMARY]
 GO
 
-CREATE TABLE [UserRoleMapping] (
+CREATE TABLE [UserRole] (
+  [id] int NOT NULL IDENTITY (1, 1) ,
   [username] varchar(20) NOT NULL ,
-  [role] varchar(20) NOT NULL
-) ON [PRIMARY]
-GO
-
-CREATE TABLE [Permissions] (
-  [id] int IDENTITY (1, 1) ,
-  [action] varchar(20) NOT NULL ,
-  [target] varchar(200) NOT NULL ,
+  [role] varchar(20) NOT NULL , 
  PRIMARY KEY ([id])
 ) ON [PRIMARY]
 GO
 
-CREATE TABLE [RolePermissionMapping] (
+CREATE TABLE [Permissions] (
+  [id] int NOT NULL IDENTITY (1, 1) ,
+  [action] varchar(20) ,
+  [target] varchar(200) , 
+ PRIMARY KEY ([id])
+) ON [PRIMARY]
+GO
+
+CREATE TABLE [RolePermission] (
+  [id] int NOT NULL IDENTITY (1, 1) ,
   [role_name] varchar(20) NOT NULL ,
-  [permission_id] int NOT NULL
+  [permission_id] int NOT NULL , 
+ PRIMARY KEY ([id])
 ) ON [PRIMARY]
 GO
 
 CREATE TABLE [Prices] (
   [established_date] datetime NOT NULL ,
-  [price_per_kilogram] money NOT NULL ,
+  [price_per_kilogram] money NOT NULL , 
  PRIMARY KEY ([established_date])
 ) ON [PRIMARY]
 GO
 
 CREATE TABLE [Log] (
-  [timestamp] timestamp NOT NULL ,
-  [content] ntext ,
+  [timestamp] datetime NOT NULL ,
+  [content] ntext , 
  PRIMARY KEY ([timestamp])
 ) ON [PRIMARY]
 GO
 
 CREATE TABLE [WorkJournal] (
-  [date] datetime ,
-  [username] varchar(20) NOT NULL ,
-  [status] varchar(20) NOT NULL ,
-  [reason] ntext ,
+  [date] datetime NOT NULL ,
+  [username] varchar(20) ,
+  [presence] varchar(20) ,
+  [reason] ntext , 
  PRIMARY KEY ([date])
 ) ON [PRIMARY]
 GO
 
+CREATE TABLE [PresenceStatuses] (
+  [presence] varchar(20) NOT NULL , 
+ PRIMARY KEY ([presence])
+) ON [PRIMARY]
+GO
+
+CREATE TABLE [OrderStatuses] (
+  [status] varchar(20) NOT NULL , 
+ PRIMARY KEY ([status])
+) ON [PRIMARY]
+GO
+
 ALTER TABLE [Orders] ADD FOREIGN KEY (creator_id) REFERENCES [Users] ([username]);
-
+				
 ALTER TABLE [Orders] ADD FOREIGN KEY (deliverer_id) REFERENCES [Users] ([username]);
-
-ALTER TABLE [UserRoleMapping] ADD FOREIGN KEY (username) REFERENCES [Users] ([username]);
-
-ALTER TABLE [UserRoleMapping] ADD FOREIGN KEY (role) REFERENCES [Roles] ([name]);
-
-ALTER TABLE [RolePermissionMapping] ADD FOREIGN KEY (role_name) REFERENCES [Roles] ([name]);
-
-ALTER TABLE [RolePermissionMapping] ADD FOREIGN KEY (permission_id) REFERENCES [Permissions] ([id]);
-
+				
+ALTER TABLE [Orders] ADD FOREIGN KEY (status) REFERENCES [OrderStatuses] ([status]);
+				
+ALTER TABLE [UserRole] ADD FOREIGN KEY (username) REFERENCES [Users] ([username]);
+				
+ALTER TABLE [UserRole] ADD FOREIGN KEY (role) REFERENCES [Roles] ([name]);
+				
+ALTER TABLE [RolePermission] ADD FOREIGN KEY (role_name) REFERENCES [Roles] ([name]);
+				
+ALTER TABLE [RolePermission] ADD FOREIGN KEY (permission_id) REFERENCES [Permissions] ([id]);
+				
 ALTER TABLE [WorkJournal] ADD FOREIGN KEY (username) REFERENCES [Users] ([username]);
+				
+ALTER TABLE [WorkJournal] ADD FOREIGN KEY (presence) REFERENCES [PresenceStatuses] ([presence]);
+		
+		
+-- Custom data
+				
+INSERT INTO [Users] VALUES ('admin', 'Administrator', 'Mr.', 'I\'m the admin', '1234')
+INSERT INTO [Users] VALUES ('receptionist', 'Receptionist', 'Mrs.', 'I\'m the receptionist', '1234')
+INSERT INTO [Users] VALUES ('deliverer', 'Deliverer', 'Mr.', 'I\'m the deliverer', '1234')
 
+INSERT INTO [OrderStatuses] VALUES ('DELIVERED')
+INSERT INTO [OrderStatuses] VALUES ('REJECTED')
+INSERT INTO [OrderStatuses] VALUES ('PENDING')
+INSERT INTO [OrderStatuses] VALUES ('CONFIRMED')
+INSERT INTO [OrderStatuses] VALUES ('DELIVERING')
 
+INSERT INTO [PresenceStatuses] VALUES ('PRESENT')
+INSERT INTO [PresenceStatuses] VALUES ('ABSENT')
+INSERT INTO [PresenceStatuses] VALUES ('ONLEAVE')
